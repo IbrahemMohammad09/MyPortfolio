45b1bf5bf98fc87b8e59a670fa66a3dab794c5a0
IbrahemMohammad09 <137792138+IbrahemMohammad09@users.noreply.github.com>
2025-09-30 00:43:03 +0300

    add hero section

--- IbrhemPortfolio/src/App.jsx
+++ IbrhemPortfolio/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { ThemeProvider } from './context/ThemeContext'
 import Navbar from './components/Navbar'
+import HeroSections from './components/Sections/HeroSections'
 
 const App = () => {
   return (
       <ThemeProvider>
         <div>
           <Navbar />
+          <HeroSections />
         </div>
       </ThemeProvider>
 
